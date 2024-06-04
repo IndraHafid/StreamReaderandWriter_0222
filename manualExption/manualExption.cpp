b49209d9c9e45e34395dd7b8a2cc16e754feb7c2
IndraHfid <indrahafidsaputra@gmail.com>
2024-06-04 15:29:27 +0700

    jika selain integer maka blognya aka dieksekusi

---
+++ manualExption/manualExption.cpp
@@ -0,0 +1,33 @@
+// manualExption.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        throw 0.5;
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
